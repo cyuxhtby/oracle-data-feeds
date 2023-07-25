06268b47847326e6b7aeada93c555d0d5e3de1ba
Diego Aguilar <d.aguilaro99@gmail.com>
2023-07-24 23:38:51 -0500

    set compiler to 0.8.18

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { PRIVATE_KEY, INFURA_API_KEY } = process.env;
 
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.18",
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
